262eaf42fa9affd7f0a7590b95451f643e548f5a
Connor Yates <connor.t.yates@gmail.com>
2016-10-23 15:55:08 +0000

    Remove debug statement

--- lib/API/Routes/Correlate.pm
+++ lib/API/Routes/Correlate.pm
@@ -22,14 +22,12 @@ post '/api/v1/correlate/bull_breese/peptide_length' => sub {
     send_error("Missing required arg : peptide_length" => 400)
       unless defined $params->{peptide_length};
 
-    use Data::Dumper;
     my $correlation;
     try {
         $correlation = correlate_manager()->correlate_peptides({
             peptide_length => $params->{peptide_length},
         });
     } catch {
-        print Dumper $_;
         die "Failed to get correlation data : $_";
     };
 
@@ -45,6 +43,8 @@ post /api/v1/correlate/bull_breese/peptide_length
 
 REST route to handle correlation data. Accepts a parameter "peptide_length" that can either
 be a the length of a peptide or "all" which will correlate peptide of all lengths
+
+For some reason, JSON serialization fails unless $correlation is forced into string context...
 
 =cut
 
